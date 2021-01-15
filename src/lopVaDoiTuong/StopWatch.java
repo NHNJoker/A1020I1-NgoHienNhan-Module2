d80f6eaa56f9313826fbda8f1d129a130402c05d
A1020I1-NgoHienNhan <ngonhanit@gmail.com>
2021-01-15 22:41:31 +0700

    bai tap opp

--- src/lopVaDoiTuong/StopWatch.java
+++ src/lopVaDoiTuong/StopWatch.java
@@ -1,5 +1,40 @@
 package lopVaDoiTuong;
 
 public class StopWatch {
-    
+    private int startTime;
+    private int endTime;
+
+    public StopWatch() {
+
+    }
+
+    public StopWatch(int endTime) {
+        this.startTime = (int) System.currentTimeMillis();
+        this.endTime = endTime;
+    }
+
+    public int getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(int startTime) {
+        this.startTime = startTime;
+    }
+
+    public int getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(int endTime) {
+        this.endTime = endTime;
+    }
+    public void start (){
+        this.startTime = (int) System.currentTimeMillis();
+    }
+    public void end (){
+        this.endTime = (int) System.currentTimeMillis();
+    }
+    public int getElapsedTime(){
+        return endTime-startTime;
+    }
 }
