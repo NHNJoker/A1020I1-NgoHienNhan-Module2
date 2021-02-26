ba52f9d424d8fc24b209d0ca4af14c81ebf3ff95
NHNJoker <ngonhanit@gmail.com>
2021-02-26 20:59:12 +0700

    baitap

---
+++ BaiTapModule2/src/dsa_stack_queue/Employee.java
@@ -0,0 +1,110 @@
+package dsa_stack_queue;
+
+public class Employee {
+    private String nameOfEmployee;
+    private String age;
+    private String gender;
+    private String dateOfBirth;
+    private String email;
+    private int id;
+    private int numOfPhone;
+    private String level;
+    private String workPosition;
+    private int salary;
+    private String addressOfEmployee;
+
+    @Override
+    public String toString() {
+        return "nameOfEmployee='" + nameOfEmployee + '\'' +
+                ", age=" + age +
+                ", addressOfEmployee='" + addressOfEmployee + '\'';
+    }
+
+    public String getNameOfEmployee() {
+        return nameOfEmployee;
+    }
+
+    public void setNameOfEmployee(String nameOfEmployee) {
+        this.nameOfEmployee = nameOfEmployee;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getAddressOfEmployee() {
+        return addressOfEmployee;
+    }
+
+    public void setAddressOfEmployee(String addressOfEmployee) {
+        this.addressOfEmployee = addressOfEmployee;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNumOfPhone() {
+        return numOfPhone;
+    }
+
+    public void setNumOfPhone(int numOfPhone) {
+        this.numOfPhone = numOfPhone;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getWorkPosition() {
+        return workPosition;
+    }
+
+    public void setWorkPosition(String workPosition) {
+        this.workPosition = workPosition;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
---
+++ BaiTapModule2/src/dsa_stack_queue/MainEmployee.java
@@ -0,0 +1,9 @@
+package dsa_stack_queue;
+
+public class MainEmployee {
+    public static void main(String[] args) {
+//        ToChucDuLieu.addNewEmployee();
+//        ToChucDuLieu.sortByDateOfBirth();
+        ToChucDuLieu.demergingQueue();
+    }
+}
